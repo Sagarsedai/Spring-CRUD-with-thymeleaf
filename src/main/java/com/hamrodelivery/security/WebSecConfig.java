db0d1615ad41b586a6ca88a01bce42b1193d95ee
SAGAR SEDAI <leokingsagarbro@gmail.com>
2021-03-20 21:57:50 +0545

    Second commit

--- src/main/java/com/hamrodelivery/security/WebSecConfig.java
+++ src/main/java/com/hamrodelivery/security/WebSecConfig.java
@@ -2,24 +2,29 @@ package com.hamrodelivery.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
+@Configuration
 @EnableWebSecurity
 public class WebSecConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
+//  Bcrypt password encoder is used as password encoding algorithm
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(10);
     }
 
+//    This is inmemoryauthentication scheme
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().passwordEncoder(bCryptPasswordEncoder)
@@ -29,8 +34,20 @@ public class WebSecConfig extends WebSecurityConfigurerAdapter {
         ;
     }
 
+//    This is added later by me to allow all methods ignoring of spring security issues
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS,"/**");
+    }
+
+//    This is added to disable cors problem ok
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+        .formLogin()
+        .and()
+        .logout()
+        .logoutSuccessUrl("/login");
     }
+
 }
